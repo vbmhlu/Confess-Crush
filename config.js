156693263e5244af979cb474ce730334f4602bd3
HaiZukon <71865167+HaiZukon@users.noreply.github.com>
2021-09-03 17:25:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Gửi cho ... <3",
-    introTitle: 'Tên người yêu bạn à!',
+    titleWeb: "Demo",
+    introTitle: 'Nguyen van A',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
