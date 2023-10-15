6db641c0b424f6dbeecc35c40c4b6991bb2db77c
vbmhlu <96919365+vbmhlu@users.noreply.github.com>
2023-10-15 20:08:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     titleWeb: "",
-    introTitle: 'kbgfbbgfu: Vũ Hà My',
-    introDesc: `Anh xin lỗi em vợ dấu iu nhìu nhìu nhắm, bây giờ anh hứa với vợ anh nói là làm mà ko nói cũng làm ạ, anh ko là vợ iu hụt hẫng nữa âu huhu méo meo`,
+    introTitle: 'Kính gửi em bé Đỗ Thị Thu Cúc',
+    introDesc: `Anh xin lỗi em bé iu nhìu nhìu lắm lắm `,
     btnIntro: '^^HiHi^^',
-    title: 'ANH YÊU VỢ NHẮMM 🥰',
-    desc: 'Vợ iu tha lỗi cho anh nháaaaa ',
-    btnYes: 'óki <33',
-    btnNo: 'ko',
-    question: 'vợ iu mún anh làm gì cũng được hớt <3',
+    title: 'ANH YÊU EM VÃ LÒN 🥰',
+    desc: 'EM CÓ IU ANH KO ',
+    btnYes: 'em có iu anh <33',
+    btnNo: 'đếu',
+    question: 'tuần sau i chơi với anh nhé <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'tuần sau mik i chưi nha <33333333',
+    reply: 'dạ anh  <33333333',
     mess: 'anh yêu em lắm',
-    messDesc: 'mai nha vợ yêu.',
-    btnAccept: 'Okiiiii lun <3', 
+    messDesc: 'hihihihi',
+    btnAccept: 'i love u <3', 
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
   
